baa151c9c356fd1dfa37a3a9d20f4e5662d8b73c
Joey Rosztoczy <joeyr@stotzeq.com>
2015-09-22 21:25:11 -0700

    jeff still sucks

--- bob/bob.exs
+++ bob/bob.exs
@@ -3,26 +3,24 @@ defmodule Teenager do
     input 
     |> listen
     |> respond
-
   end
 
   defp listen(input) do
     cond do
-        String.ends_with? input, "?" -> :question
-        Regex.run(~r/(\S)/, input) == nil -> :silence
-        String.upcase(input) == input &&
-        String.downcase(input) != input -> :shouting
-        true -> true
+      String.ends_with? input, "?" -> :question
+      Regex.run(~r/(\S)/, input) == nil -> :silence
+      String.upcase(input) == input &&
+      String.downcase(input) != input -> :shouting
+      true -> true
     end
   end
 
   defp respond(msg) do
     case msg do
-    :question -> "Sure."
-    :silence -> "Fine. Be that way!"
-    :shouting -> "Whoa, chill out!"
-    _ -> "Whatever."
+      :question -> "Sure."
+      :silence -> "Fine. Be that way!"
+      :shouting -> "Whoa, chill out!"
+      _ -> "Whatever."
     end
-
   end
 end
